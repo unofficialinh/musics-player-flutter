4662c9eba51d61d52f446f05c78124cfb37314fa
unofficialinh <nguyenthuylinh3400@gmail.com>
2021-05-13 09:08:36 +0700

    empty playing list constructor

--- lib/model/PlayingListModel.dart
+++ lib/model/PlayingListModel.dart
@@ -5,40 +5,10 @@ class PlayingListModel extends ChangeNotifier {
   List<dynamic> _songs;
   AudioPlayer audioPlayer;
   ConcatenatingAudioSource audioSource;
-  PlayingListModel() {
-     // TODO: add some sample songs, delete later
-    _songs = [
-      {
-        "id": 1,
-        "title": "To the moon",
-        "img": "http://10.0.2.2:5000/img/to-the-moon.jpg",
-        "mp3": "http://10.0.2.2:5000/mp3/to-the-moon.mp3",
-        "artist_id": 1,
-        "artist": "Hooligan",
-        "album_id": 1,
-        "album": "To the moon",
-        "lyrics": "Lyric ne"
-      },
-      {
-        "album": "I met you when I was 18.",
-        "album_id": 2,
-        "artist": "Lauv",
-        "artist_id": 2,
-        "id": 2,
-        "img": "http://10.0.2.2:5000/img/paris-in-the-rain.jpg",
-        "lyrics": "Lyric ne",
-        "mp3": "http://10.0.2.2:5000/mp3/paris-in-the-rain.mp3",
-        "title": "Paris in the rain"
-      },
-    ];
-    audioSource = ConcatenatingAudioSource(
-        children: List.generate(_songs.length, (index) => AudioSource.uri(Uri.parse(_songs[index]['mp3'])))
-    );
-    // endTODO
 
+  PlayingListModel() {
+    _songs = [];
     audioPlayer = new AudioPlayer();
-    audioPlayer.setAudioSource(audioSource);
-    notifyListeners();
   }
 
   // p.songs to get
@@ -63,6 +33,12 @@ class PlayingListModel extends ChangeNotifier {
 
   // Return false if song exist, true if add success
   bool addBack(dynamic newSong) {
+    if (_songs.length == 0) {
+      audioSource = ConcatenatingAudioSource(
+          children: List.generate(_songs.length, (index) => AudioSource.uri(Uri.parse(_songs[index]['mp3'])))
+      );
+      audioPlayer.setAudioSource(audioSource);
+    }
     if (!exist(newSong)) {
       _songs.add(newSong);
       audioSource.add(AudioSource.uri(Uri.parse(newSong['mp3'])));
